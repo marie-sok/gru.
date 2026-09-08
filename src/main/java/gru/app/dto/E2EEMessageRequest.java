f9f9cab4b520ef01f33d3fec4dba26596193f082
Marie Sok <sarrahmcfarlend@icloud.com>
2026-09-09 00:16:44 +0300

    security(backend): add encrypted message request DTO

---
+++ src/main/java/gru/app/dto/E2EEMessageRequest.java
@@ -0,0 +1,13 @@
+package gru.app.dto;
+
+import lombok.Data;
+
+@Data
+public class E2EEMessageRequest {
+    private String chatId;
+    private String encryptedPayload;
+    private String encryptionVersion;
+    private String senderEphemeralPublicKey;
+    private String signature;
+    private String senderKeyFingerprint;
+}
