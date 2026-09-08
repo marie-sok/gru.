adf919f7785539564676df66fdb037379dcf0b2a
Marie Sok <sarrahmcfarlend@icloud.com>
2026-09-09 00:56:12 +0300

    security(e2ee): add replay lookup

--- src/main/java/gru/app/repository/MessageRepository.java
+++ src/main/java/gru/app/repository/MessageRepository.java
@@ -4,10 +4,12 @@ import gru.app.model.Message;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MessageRepository
         extends MongoRepository<Message, String> {
 
     List<Message> findByChatIdOrderByCreatedAtAsc(String chatId);
     List<Message> findBySenderId(String senderId);
+    Optional<Message> findFirstBySenderIdAndE2eeClientMessageId(String senderId, String e2eeClientMessageId);
 }
