6e84f0fb5efb29099b5e50ae46cb7e63baee04c4
Marie Sok <sarrahmcfarlend@icloud.com>
2026-09-09 01:20:15 +0300

    security(e2ee): expose encrypted edit endpoint

--- src/main/java/gru/app/controller/E2EEMessageController.java
+++ src/main/java/gru/app/controller/E2EEMessageController.java
@@ -6,6 +6,8 @@ import gru.app.service.E2EEMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +25,22 @@ public class E2EEMessageController {
             @RequestBody E2EEMessageRequest request
     ) {
         Message message = e2eeMessageService.send(authentication.getName(), request);
-        messagingTemplate.convertAndSend("/topic/chat/" + message.getChatId(), message);
+        broadcast(message);
         return message;
+    }
+
+    @PatchMapping("/messages/{messageId}/e2ee")
+    public Message edit(
+            Authentication authentication,
+            @PathVariable String messageId,
+            @RequestBody E2EEMessageRequest request
+    ) {
+        Message message = e2eeMessageService.edit(authentication.getName(), messageId, request);
+        broadcast(message);
+        return message;
+    }
+
+    private void broadcast(Message message) {
+        messagingTemplate.convertAndSend("/topic/chat/" + message.getChatId(), message);
     }
 }
